593768ff8e2810f59b170dcac961c059037f4d75
EvanM19 <evanmoreau19@gmail.com>
2025-06-27 15:18:35 +0200

    ajout data avec variables

--- creation_variables.py
+++ creation_variables.py
@@ -11,17 +11,31 @@ test = pd.read_csv('data/X_test.csv')
 # CASH_PRICE1 A CASH_PRICE24 (NUM) 	Prix de l'item 1 à 24 	850 
 # MAKE1 A MAKE24 (CHAR) 	Fabriquant de l'item 1 à 24 	Apple 
 # MODEL1 A MODEL24 (CHAR) 	Description du modèle de l'item 1 à 24 	Apple Iphone XX 
-# GOODS_CODE1 A GOODS_CODE24 (CHAR) 	Code de l'item 1 à 24 	2378284364 
+# goods_code1 A goods_code24 (CHAR) 	Code de l'item 1 à 24 	2378284364 
 # NBR_OF_PROD_PURCHAS1 À NBR_OF_PROD_PURCHAS24 (NUM) 	Nombre de produits dans l'item 1 à 24 	2 
-# NB_OF_ITEMS (NUM) 	Nombre total d'items 	7 
+# Nb_of_items(NUM) 	Nombre total d'items 	7 
 
-# Enlever GOODS_CODE
-train = train.drop(columns=['GOODS_CODE'])
-test = test.drop(columns=['GOODS_CODE'])
+# Enlever goods_code
+for i in range(1, 25):
+    train = train.drop(columns=[f'goods_code{i}'])
+    test = test.drop(columns=[f'goods_code{i}'])
 
-# Création de variables
+## Création de variables
 
 # Prix total par items : Prix de chaque item multiplié par le nombre d'items
 for i in range(1, 25):
-    train[f'TOTAL_PRICE{i}'] = train[f'CASH_PRICE{i}'] * train[f'NBR_OF_PROD_PURCHAS{i}']
-    test[f'TOTAL_PRICE{i}'] = test[f'CASH_PRICE{i}'] * test[f'NBR_OF_PROD_PURCHAS{i}']
+    train[f'TOTAL_PRICE{i}'] = train[f'cash_price{i}'] * train[f'Nbr_of_prod_purchas{i}']
+    test[f'TOTAL_PRICE{i}'] = test[f'cash_price{i}'] * test[f'Nbr_of_prod_purchas{i}']
+
+# Prix total de tous les items
+train['TOTAL_PRICE'] = train[[f'TOTAL_PRICE{i}' for i in range(1, 25)]].sum(axis=1)
+test['TOTAL_PRICE'] = test[[f'TOTAL_PRICE{i}' for i in range(1, 25)]].sum(axis=1)
+
+# Moyenne des prix par item : TOTAL_PRICE divisé par Nb_of_items 
+train['MEAN_PRICE'] = train['TOTAL_PRICE'] / train['Nb_of_items']
+test['MEAN_PRICE'] = test['TOTAL_PRICE'] / test['Nb_of_items']
+
+# enregistrer en CSV
+train.to_csv('data/X_train_variables.csv', index=False)
+test.to_csv('data/X_test_variables.csv', index=False)
+
